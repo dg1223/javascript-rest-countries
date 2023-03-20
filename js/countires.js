229d16d9e54f0d5641d23bd45fc05d660d797a9e
dg1223 <alavi1223@hotmail.com>
2023-03-20 01:11:28 -0400

    different options in destructuring

--- js/countires.js
+++ js/countires.js
@@ -5,19 +5,39 @@ const loadCountries = () => {
 };
 
 const displayCountries = (countries) => {
+  console.log(countries[0]);
   const countriesHTML = countries.map((country) => getCountryHTML(country));
-  //   console.log(countriesHTML[0]);
   const container = document.getElementById("countries");
   container.innerHTML = countriesHTML.join(" ");
 };
 
-const getCountryHTML = (country) => {
+// const getCountryHTML = (country) => {
+// option 1: general
+//   return `
+//         <div class="country">
+//             <h2>${country.name.common}</h2>
+//             <img src="${country.flags.png}">
+//         </div>
+//     `;
+
+// option 2: destructuring
+//   const { name, flags } = country;
+//   return `
+//         <div class="country">
+//             <h2>${name.common}</h2>
+//             <img src="${flags.png}">
+//         </div>
+//     `;
+// };
+
+const getCountryHTML = ({ name, flags }) => {
+  // option 3: parameter destructuring
   return `
-        <div class="country">
-            <h2>${country.name.common}</h2>
-            <img src="${country.flags.png}">
-        </div>
-    `;
+          <div class="country">
+              <h2>${name.common}</h2>
+              <img src="${flags.png}">
+          </div>
+      `;
 };
 
 loadCountries();
